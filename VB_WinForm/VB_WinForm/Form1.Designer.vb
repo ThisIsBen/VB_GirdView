0cb7fc5a49a2e1fc7c558f940d18bfcdca509f96
ThisIsBen <beanchen2002@gmail.com>
2023-04-16 15:32:47 +0900

    Display a list content in GridView in VB

---
+++ VB_WinForm/VB_WinForm/Form1.Designer.vb
@@ -0,0 +1,55 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請勿使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.CurrentFolder_GV = New System.Windows.Forms.DataGridView()
+        CType(Me.CurrentFolder_GV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'CurrentFolder_GV
+        '
+        Me.CurrentFolder_GV.AllowUserToAddRows = False
+        Me.CurrentFolder_GV.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.DisplayedCells
+        Me.CurrentFolder_GV.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCells
+        Me.CurrentFolder_GV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.CurrentFolder_GV.Location = New System.Drawing.Point(33, 67)
+        Me.CurrentFolder_GV.Name = "CurrentFolder_GV"
+        Me.CurrentFolder_GV.RowTemplate.Height = 24
+        Me.CurrentFolder_GV.Size = New System.Drawing.Size(368, 90)
+        Me.CurrentFolder_GV.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(676, 395)
+        Me.Controls.Add(Me.CurrentFolder_GV)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.CurrentFolder_GV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents CurrentFolder_GV As DataGridView
+End Class
---
+++ VB_WinForm/VB_WinForm/Form1.vb
@@ -0,0 +1,91 @@
+﻿Public Class Form1
+
+
+    Private Sub Form1_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+
+
+
+
+        '"現在作業中の対象"Columnを作成
+        Dim ColImage As New DataGridViewImageColumn
+        ColImage.Name = "CurrentProgress"
+        'Set Header text
+        ColImage.HeaderText = "現在作業中の対象"
+        'Set column width
+        ColImage.Width = 100
+        'Add column to CurrentFolder_GV
+        CurrentFolder_GV.Columns.Add(ColImage)
+
+
+
+
+
+        '"本日の精度監視対象"Columnを作成
+        Dim ColFolder As New DataGridViewTextBoxColumn
+        ColFolder.HeaderText = "本日の精度監視対象"
+        ColFolder.Name = "TargetFolder"
+        ColFolder.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill
+        ColFolder.SortMode = DataGridViewColumnSortMode.NotSortable
+        'Add column to CurrentFolder_GV
+        CurrentFolder_GV.Columns.Add(ColFolder)
+
+
+
+
+        'Fill up the CurrentFolder_GV with the content of TargetFolderList
+        Dim TargetFolderList As New List(Of String)({"C:\YourFolder", "C:\YourFolder2", "C:\YourFolder3", "C:\YourFolder4"})
+        For i As Integer = 0 To TargetFolderList.Count - 1
+
+            'Create a new row 
+            Dim newRow As New DataGridViewRow
+            CurrentFolder_GV.Rows.Add(newRow)
+
+            'Set value for "現在作業中の対象"
+            'We use a white image as the default value
+            Dim arrowCell As New DataGridViewImageCell
+            arrowCell.Value = New Bitmap(1, 1)
+            'Add to the new row
+            CurrentFolder_GV.Rows(i).Cells(0) = arrowCell
+
+            'Set value for 本日の精度監視対象"
+            Dim folderCell As New DataGridViewTextBoxCell
+            folderCell.Value = TargetFolderList(i)
+            'Add to the new row
+            CurrentFolder_GV.Rows(i).Cells(1) = folderCell
+        Next
+
+
+
+        'Premise: a flag file is created for 精度監視終了フォルダー
+        '精度監視が実施されていないフォルダーを現在作業中の対象として表示する
+        Dim nextTargetFolderIndex = 1
+        scrollToCurrentProgress(nextTargetFolderIndex)
+
+
+
+
+
+
+    End Sub
+
+    '精度監視が実施されていないフォルダーを現在作業中の対象として表示する
+    Private Sub scrollToCurrentProgress(rowNo)
+        CurrentFolder_GV.Rows(rowNo).Cells(0).Value = Image.FromFile("./arrow.png")
+
+        CurrentFolder_GV.CurrentCell = CurrentFolder_GV.Rows(rowNo).Cells(0)
+        CurrentFolder_GV.Rows(rowNo).DefaultCellStyle.BackColor = System.Drawing.Color.Orange
+
+
+    End Sub
+
+    'Disable editing of this gridview
+    Private Sub CurrentFolder_GV_SelectionChanged(sender As System.Object, e As System.EventArgs) Handles CurrentFolder_GV.SelectionChanged
+        CurrentFolder_GV.ClearSelection()
+
+    End Sub
+
+    'Hide the leftmost auto-generated row header 
+    Private Sub Form1_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        CurrentFolder_GV.RowHeadersVisible = False
+    End Sub
+End Class
