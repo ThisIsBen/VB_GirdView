8d39dcf52f41c26954e84dccc106d0df13b7b67f
Ben <q36064010@gs.ncku.edu.tw>
2023-12-19 22:30:05 +0900

    Add files via upload

---
+++ 2023FinalStruggle/GlobalConstants.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace [TBD]
+{
+    public class GlobalConstants
+    {
+       
+        #region 処理時間測定記録の出力
+        public static string OveralTimeFilePath = @"C:\AutoCompressorWindowsService\ユーザー操作\圧縮済みフォルダー記録.json";
+        public static string DetailedTimeFilePath = @"C:\AutoCompressorWindowsService\ユーザー操作\圧縮済みフォルダー記録.json";
+
+        #endregion
+
+
+
+        #region エラーメッセージの出力
+        public static string errorMessageTxtFolderPath = @"C:\AutoCompressorWindowsService\ユーザー操作\圧縮済みフォルダー記録.json";
+
+        #endregion
+
+
+       
+
+
+
+
+    }
+}
