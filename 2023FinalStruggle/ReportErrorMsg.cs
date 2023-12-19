4e6ac2c82d76220527b0f0a00db4f932ec51c631
Ben <q36064010@gs.ncku.edu.tw>
2023-12-19 22:11:43 +0900

    Add files via upload

---
+++ 2023FinalStruggle/ReportErrorMsg.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace App
+{
+    class ReportErrorMsg
+    {
+       
+
+        //To display error message to the user
+        //Async output error message to a txt file in エラーメッセージ folder in NAS 
+        public static async void outputErrorMsg(string errorMessage,string errorType="", bool needAccumulate=false)
+        {
+            
+            try
+            {
+                string errorFilePath="";
+                bool append;
+                if(needAccumulate)
+                {
+                    errorFilePath=GlobalConstants.errorMessageTxtFolderPath + "\\" + DateTime.Now.ToString("yyyy_MM_dd") + errorType + ".txt";
+                    append=true;
+                }
+                else
+                {
+                    errorFilePath=GlobalConstants.errorMessageTxtFolderPath + "\\" + DateTime.Now.ToString("yyyy_MM_dd--HH_mm_ss") + errorType + ".txt"
+                    append=false;
+                }
+                using (StreamWriter writer = new StreamWriter(errorFilePath, append))
+                {
+                    await writer.WriteAsync(errorMessage);
+                }
+            }
+            catch
+            {
+                return;
+            }
+           
+           
+        }
+
+        //Display message with a pop-up message box
+        public static IntPtr WTS_CURRENT_SERVER_HANDLE = IntPtr.Zero;
+
+        //Display message with a pop-up window
+        //[Use this function if the program is going to be shut down after displaying this pop-up message]
+        //For example, use a pop-up window to display a error message when the exe is going to 
+        //shut down automatically due to the error.
+        public static void showMsgBox_Anyway(string message, string title)
+        {
+            //エラーメッセージを本アプリが使っている文字コードに変換し、日本語パスの文字化けを解決する。
+            message = GlobalConstants.AppEncoding.GetString(GlobalConstants.AppEncoding.GetBytes(message));
+            //Plan B: message = GlobalConstants.AppEncoding.GetString(Encoding.Convert(Encoding.UTF8, GlobalConstants.AppEncoding, Encoding.UTF8.GetBytes(message)));
+            int resp = 0; WTSSendMessage(WTS_CURRENT_SERVER_HANDLE, WTSGetActiveConsoleSessionId(), title, title.Length, message, message.Length, 0, 0, out resp, false);
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)] public static extern int WTSGetActiveConsoleSessionId();[DllImport("wtsapi32.dll", SetLastError = true)] public static extern bool WTSSendMessage(IntPtr hServer, int SessionId, String pTitle, int TitleLength, String pMessage, int MessageLength, int Style, int Timeout, out int pResponse, bool bWait);
+
+
+
+
+
+        //Only display error message with a pop-up window when no pop-up window is being shown.
+        //[Use this function if the program will still running after displaying this pop-up message.]
+        //For example, when displaying "画像を処理できない"、"カメラが移動された"　error message
+        //with a pop-up window. 
+        //Because these 画像状態エラー are likely to occur every second until the problem is fixed.
+
+        //Record the ID of the csrss that shows message box
+        private static int showMsgBox_csrssIndex = -1;
+        public static void showMsgBox_IfNotShown(string message, string title)
+        {
+
+            //We show message box if it is the first time error message box is shown.
+            if (showMsgBox_csrssIndex == -1)
+            {
+
+
+                //show the message box 
+                showMsgBox_Anyway(message, title);
+
+                //Get the Process ID of the csrss.exe that shows the error message box
+                Process[] csrssProcess = Process.GetProcessesByName("csrss");
+
+
+                for (byte i = 0; i < csrssProcess.Length; i++)
+                {
+                    if (csrssProcess[i].MainWindowTitle != "")
+                    {
+
+
+                        showMsgBox_csrssIndex = csrssProcess[i].Id;
+
+                    }
+
+
+                }
+
+            }
+
+            //If it is not the first time error message box is shown,
+            //we check if the csrss.exe that shows the error message box is showing an error message box right now,
+            //if so, we do not show the message box.
+            //if not, we show the message box.
+            else
+            {
+                Process showMsgBox_csrssProcess = Process.GetProcessById(showMsgBox_csrssIndex);
+
+
+                if (showMsgBox_csrssProcess.MainWindowTitle == "")
+                {
+                    showMsgBox_Anyway(message, title);
+
+                }
+
+
+
+
+            }
+
+
+        }
+
+        //Display message with a pop-up window if currently no pop-up window is shown and upload the error message to NAS
+        public static void showMsgBoxIfNotShown_UploadErrMsgToNAS(string errorMessage, string errorMessageBoxTitle, string NASErrorTxtFileName)
+        {
+            // MessageBoxでオペレーターに知らせ、再起動してもらう。
+            //show the error message box to inform the user if the same message box is not being shown 
+            ReportErrorMsg.showMsgBox_IfNotShown(errorMessage, errorMessageBoxTitle);
+
+            //output the error message  
+            //to the アプリ_エラーメッセージ folder on NAS
+            ReportErrorMsg.outputErrorMsg(NASErrorTxtFileName, errorMessage);
+
+            Console.WriteLine(errorMessage);
+        }
+    }
+}
---
+++ 2023FinalStruggle/SendDataToWebServer.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace WpfSendHttpRequest
+{
+    class SendDataToWebServer
+    {
+        
+
+        //[Read server URL from setting file]
+        private static string webServer = "http://localhost:62338/Home/GetClientData";
+
+        
+        private static readonly HttpClient client = new HttpClient();
+        //Webサーバーに送信する
+        //送信内容を一つのdictionaryに入れる必要がある。
+        public async void sendData(Dictionary<string, string> clientMsgDict)
+        {
+            //var Example_clientMsgDict = new Dictionary<string, string>
+            //  {
+                 
+            //      { "key1", "10.jpg" },
+            //      { "key2", "NG" }
+            //  };
+
+
+
+            try
+            {
+                //Reference:https://copyprogramming.com/howto/csharp-send-json-in-post-request-c-httpclient#send-json-data-in-http-post-request-c
+                string jsonString = JsonConvert.SerializeObject(clientMsgDict);
+                var content = new StringContent(jsonString, Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(webServer, content);
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseString = await response.Content.ReadAsStringAsync();
+
+                    //If the content returned is in json
+                    string serverMsg="";
+                    try
+                    {
+                        
+                        //Get the response as an object to get the value directly 
+                        //if the response contains JSON information 
+                        var responseObj = (ServerResponse)JsonConvert.DeserializeObject(responseString);
+
+                        //Extract values from the server response
+                        var age = responseObj.serverStatus;
+                        var name = responseObj.sendError;
+                        serverMsg = "Server response format: JSON\n";
+                        serverMsg += $"{age}{name}";
+                       
+                    }
+
+                    //If the response from the server is not in JSON format, 
+                    //read it as a string
+                    catch (Exception)
+                    {
+                        serverMsg = responseString;
+                    }
+
+                    //For Debug
+                    //Write the response from server to log for debug
+                    string errorType = "WebServerに送信成功記録";
+                    ReportErrorMsg.outputErrorMsg(serverMsg, errorType);
+
+                }
+            }
+            catch(Exception e)
+            {
+                //Output and accumulate the error message to a file 
+                string errorType = "WebServerに送信エラー";
+                ReportErrorMsg.outputErrorMsg(e.StackTrace, errorType, needAccumulate: true);
+
+            }
+
+
+        }
+
+
+        // The JSON data received from the server
+        public class ServerResponse
+        {
+            public string sendError { get; set; }
+            public string serverStatus { get; set; }
+        }
+    }
+}
